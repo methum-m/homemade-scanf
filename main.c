73c99fd00866e2054b352fe88045dbdf98ff5487
Methum Menthusa <moneysenseed@gmail.com>
2026-01-18 23:40:14 +0530

    Implemented basic number validation

--- main.c
+++ main.c
@@ -4,8 +4,11 @@ int main(int argc, char *argv[]) {
 
   int input = getchar();
 
-  if (input <= 57 && input >= 48) {
-    printf("%d\n", input);
-    printf("%s\n", "This is a number value");
+  while (input >= '0' && input <= '9') {
+
+    printf("%s\n", "This is a number");
+    return input;
   }
+
+  printf("%s\n", "This is not a number");
 }
